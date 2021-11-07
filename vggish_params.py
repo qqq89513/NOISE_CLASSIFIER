7638ac16209ef503289c8f7a718c15e718834211
ghnmqdtg <ghnmqdtg@gmail.com>
2021-11-07 14:43:57 +0800

    Misc: Modify file paths

--- vggish_params.py
+++ vggish_params.py
@@ -57,5 +57,5 @@ OUTPUT_TENSOR_NAME = OUTPUT_OP_NAME + ':0'
 AUDIO_EMBEDDING_FEATURE_NAME = 'audio_embedding'
 
 # Paths
-PATH_NOISE_ROOT = './QC_noise/'
-PATH_NOISE_LIST = './QC_noise/QC_noise_filenames.json'
+PATH_NOISE_ROOT = './Dataset/'
+PATH_NOISE_LIST = './Dataset/Noise_filenames.json'
